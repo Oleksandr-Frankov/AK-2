081e9ced55d22a8138ed4d983bf35ff106805688
Oleksandr-Frankov <66685590+Oleksandr-Frankov@users.noreply.github.com>
2022-01-13 17:35:48 +0000

    Add files via upload

--- lab6/hello.c
+++ lab6/hello.c
@@ -1,94 +1,66 @@
-// SPDX-License-Identifier: GPL-2.0+
 #include <linux/init.h>
 #include <linux/module.h>
-#include <linux/moduleparam.h>
 #include <linux/printk.h>
-#include <linux/types.h>
-
+#include <linux/moduleparam.h>
 #include <linux/ktime.h>
 #include <linux/slab.h>
-#include <linux/list.h>
+#include <asm-generic/bug.h>
 
-struct list_item {
-	struct list_head next;
-	ktime_t start_time;
-	ktime_t finish_time;
-};
-
-MODULE_AUTHOR("Frankov Oleksandr <sfrankov2@gmail.com>");
-MODULE_DESCRIPTION("Hello, world in Linux Kernel Training");
+MODULE_AUTHOR("Frankov O.O. <frankov@gmail.com>");
+MODULE_DESCRIPTION("Lab 6");
 MODULE_LICENSE("Dual BSD/GPL");
-MODULE_VERSION("3.0");
-
-static LIST_HEAD(head);
 
 static uint count = 1;
+
 module_param(count, uint, 0444);
-MODULE_PARM_DESC(count, "Count to print 'Hello, world!'");
+MODULE_PARM_DESC(count, "Number of prints");
+
+struct my_struct {
+	struct list_head tlist;
+	ktime_t time;
+};
+
+static LIST_HEAD(my_list_head);
 
 static int __init hello_init(void)
-{
-	uint i = 0;
-	struct list_item *tail;
+{	
+	if (count == 0) {
+		pr_warn("The parameter is 0");
+	} else if (count >= 5 && count <= 10) {
+		pr_warn("The parameter is in [5; 10]");
+	}
 
-	pr_info("Count: %d\n", count);
+	BUG_ON((count > 10));
 
-	if (!count)
-		pr_warn("The parameter is 0. You cannot enter 0.");
+	int i;
+	for (i = 0; i <= count; i++) {
 
-	if (count >= 5 && count <= 10)
-		pr_warn("The parameter is %d, between [5;10], enter number less than 5", count);
+		struct my_struct *ms = NULL;
 
-	BUG_ON(count > 10);
+		if (i != count)
+		{
+			ms = kmalloc(sizeof(struct my_struct), GFP_KERNEL);
+		}
+		ms->time = ktime_get();
+		list_add_tail(&ms->tlist, &my_list_head);
 
-	for (i = 0; i < count; i++) {
-		tail = kmalloc(sizeof(struct list_item), GFP_KERNEL);
-
-		if (i == 5)
-			tail = 0;
-
-		if (ZERO_OR_NULL_PTR(tail))
-			goto error;
-
-		tail->start_time = ktime_get();
-		pr_info("Hello, World!\n");
-		tail->finish_time = ktime_get();
-
-		list_add_tail(&(tail->next), &head);
+		pr_info("Hello, world!\n");
 	}
 
 	return 0;
-
-error:
-	{
-		struct list_item *md, *tmp;
-
-		pr_err("Out of memory!\n");
-
-		list_for_each_entry_safe(md, tmp, &head, next) {
-			list_del(&md->next);
-			kfree(md);
-		}
-
-		BUG();
-		return -ENOMEM;
-	}
 }
-
 
 static void __exit hello_exit(void)
 {
-	struct list_item *md, *tmp;
+	int count = 0;
+	struct my_struct *t, *tmp;
 
-	list_for_each_entry_safe(md, tmp, &head, next) {
-		pr_info("Printing time: %lld",
-		md->finish_time - md->start_time);
-
-		list_del(&md->next);
-		kfree(md);
+	list_for_each_entry_safe(t, tmp, &my_list_head, tlist) {
+		pr_info("i: %d; ktime = %lld\n", count++, ktime_to_ns(t->time));
+		list_del(&t->tlist);
+		kfree(t);
 	}
-
-	BUG_ON(!list_empty(&head));
+	pr_info("Module unloaded\n");
 }
 
 module_init(hello_init);
